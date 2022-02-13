11918a3922d8d4a71b8f1fd8d6f452c8bb4ca4dc
Diarit <diarit-94@hotmail.fr>
2022-02-13 19:51:27 +0100

    dsf

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ $query->bindValue(':perPage', $perPage, PDO::PARAM_INT);
 $query->bindValue(':sort', $sort, PDO::PARAM_STR);
 $query->bindValue(':direction', $direction, PDO::PARAM_STR);
 
-var_dump($direction);
+// var_dump($direction);
 $query->execute();
 
 $movies = $query->fetchAll(PDO::FETCH_ASSOC);
