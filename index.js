28aa207b3d8589118f64bcc3ebc89dbd24bf6179
Diarit-S <Diarit-94@hotmail.fr>
2022-02-14 15:25:38 +0100

    fix headers

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 const port = process.env.PORT || 8080;
 
 app.use('/', router);
